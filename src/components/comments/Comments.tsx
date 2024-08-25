8a02e05d62f61ea6069c8e9432c24e8ffa67724d
RidaNaz <rnaz3414@gmail.com>
2024-08-25 20:42:49 +0500

    Commit by Rida

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -24,7 +24,7 @@ const Comments = ({ postSlug }:any) => {
   const { status } = useSession();
 
   const { data, mutate, isLoading } = useSWR(
-    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    `https://naz-blogs.vercel.app/api/comments?postSlug=${postSlug}`,
     fetcher
   );
 
